aa3aa777bc931ef4742c05efe8ba91dde4069aac
sivaiah <messalasivaiah5@gmail.com>
2025-09-06 20:56:38 +0530

    second front

---
+++ src/services/projectService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API = 'http://localhost:8045/api/projects';
+
+export const getAll = () => axios.get(API);
+
+export const getById = (id) => axios.get(`${API}/${id}`);
+
+export const create = (project) => axios.post(API, project);
+
+export const update = (id, project) => axios.put(`${API}/${id}`, project);
+
+export const remove = (id) => axios.delete(`${API}/${id}`);
